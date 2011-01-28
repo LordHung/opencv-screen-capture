8aeb60c4ac24b7868a7794ba6cfab5f6ccea94ab
janne <janne.raiskila@gmail.com>
2011-01-28 09:23:22 +0200

    init

---
+++ screenCapture.h
@@ -0,0 +1,71 @@
+/*****************************************
+ * Screen Capture for OpenCV (>= 2.1)    *
+ * by JR                                 *
+ *****************************************/
+
+/**********************************************************************
+ ** Quick usage:                                                      *
+ *                                                                    *
+ * WindowCapture cap(hWnd);                                           *
+ * Mat dst;                                                           *
+ * cap.captureFrame(dst);                                             *
+ *                                                                    *
+ * Use getHwndsByProcessName() to get a list of the window handles    *
+ * (hWnds) for a given process.                                       *
+ *                                                                    *
+ * To capture a DirectX/OpenGL-rendered window, set the 'screenshot'  *
+ * parameter on the constructor to true. This will take a screenshot  *
+ * of the composed desktop and crop out the desired window.           *
+ *                                                                    *
+ **********************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <windows.h>
+#include <cv.h>
+
+using namespace std;
+using namespace cv;
+
+struct CaptureState
+{
+    HWND window;
+    int width, height;
+    RECT wr, cr;
+
+    bool screenshot;
+    HDC windowDc;
+    HDC memDc;
+    HBITMAP hbmp;
+    BITMAPINFOHEADER bi;
+};
+
+class WindowCapture
+{
+private:
+    const bool screenshot;
+    CaptureState* state;
+
+    void initialize();
+
+public:
+    const HWND window;
+
+    WindowCapture(HWND window, bool screenshot = false);
+    ~WindowCapture();
+
+    int width();
+    int height();
+    void captureFrame(Mat& dst);
+};
+
+void getHwndsByProcessId(DWORD processId, vector<HWND>& list, bool visibleOnly = true);
+void getHwndsByProcessName(const wchar_t* processName, vector<HWND>& list, bool visibleOnly = true);
+void revealWindow(HWND window);
+
+CaptureState* beginCaptureWindow(HWND window, bool screenshot = false);
+void endCaptureWindow(CaptureState*& cs);
+void captureFrame(const CaptureState* cs, Mat& dst);
+
+void captureWindow(HWND window, Mat& dst, bool screenshot = false);
